7a1a09b8f0c82b511ed336193b659b39672ad512
joon59 <joonjoon.444@gmail.com>
2024-05-02 14:48:48 +0700

    Enter the number

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,34 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ur grade");
+
+        int number = sc.nextInt();
+
+        if (number <= 49) {
+            System.out.println("grade F \n" + number);
+        }
+        else if (number > 49 && number < 50) {
+            System.out.println("grade D \n" + number);
+        }
+        else if (number >= 50 && number < 59) {
+            System.out.println("grade D+ \n" + number);
+        }
+        else if (number >= 60 && number < 64) {
+            System.out.println("grade C \n" + number);
+        }
+        else if (number >= 65 && number < 69) {
+            System.out.println("grade C+ \n" + number);
+        }
+        else if (number >= 70 && number < 74) {
+            System.out.println("grade B \n" + number);
+        }
+        else if (number >= 75 && number < 79) {
+            System.out.println("grade B+ \n" + number);
+        }
+        else if (number >= 80) {
+            System.out.println("grade A \n" + number);
+    }
     }
 }
